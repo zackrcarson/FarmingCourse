434a69e5e3694d3b2309263c23cbd60d688111e6
LordDarkenon <r.ager@hotmail.co.uk>
2020-05-02 17:02:38 +0100

    10.3 Scene Storing And Restoring

--- Assets/Scripts/Scene/SceneControllerManager.cs
+++ Assets/Scripts/Scene/SceneControllerManager.cs
@@ -51,6 +51,9 @@ public class SceneControllerManager : SingletonMonobehaviour<SceneControllerMana
         // Start fading to black and wait for it to finish before continuing.
         yield return StartCoroutine(Fade(1f));
 
+        // Store scene data
+        SaveLoadManager.Instance.StoreCurrentSceneData();
+
         // Set player position
 
         Player.Instance.gameObject.transform.position = spawnPosition;
@@ -66,6 +69,9 @@ public class SceneControllerManager : SingletonMonobehaviour<SceneControllerMana
 
         // Call after scene load event
         EventHandler.CallAfterSceneLoadEvent();
+
+        // Restore new scene data
+        SaveLoadManager.Instance.RestoreCurrentSceneData();
 
         // Start fading back in and wait for it to finish before exiting the function.
         yield return StartCoroutine(Fade(0f));
@@ -98,6 +104,9 @@ public class SceneControllerManager : SingletonMonobehaviour<SceneControllerMana
 
         // If this event has any subscribers, call it.
         EventHandler.CallAfterSceneLoadEvent();
+
+        SaveLoadManager.Instance.RestoreCurrentSceneData();
+
 
         // Once the scene is finished loading, start fading in.
         StartCoroutine(Fade(0f));

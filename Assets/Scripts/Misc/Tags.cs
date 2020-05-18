7c1244e1d6b61484ff61a7bdc8018866c956852d
LordDarkenon <r.ager@hotmail.co.uk>
2020-05-18 17:18:55 +0100

    15.3 Plant And Grow A Parsnip

--- Assets/Scripts/Misc/Tags.cs
+++ Assets/Scripts/Misc/Tags.cs
@@ -3,6 +3,7 @@
 public static class Tags 
 {
     public const string BoundsConfiner = "BoundsConfiner";
+    public const string CropsParentTransform = "CropsParentTransform";
     public const string GroundDecoration1 = "GroundDecoration1";
     public const string GroundDecoration2 = "GroundDecoration2";
 

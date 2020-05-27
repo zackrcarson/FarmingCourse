7a30f7c7947972f966e6ebb363204974a4c2925c
FarmingRPG <farmingrpg@example.com>
2020-05-27 14:18:41 +0100

    15.9 Create Oak Tree Stump

--- Assets/Scripts/Crop/Crop.cs
+++ Assets/Scripts/Crop/Crop.cs
@@ -135,6 +135,13 @@ public class Crop : MonoBehaviour
     {
         SpawnHarvestedItems(cropDetails);
 
+        // Does this crop transform into another crop
+        if (cropDetails.harvestedTransformItemCode > 0)
+        {
+            CreateHarvestedTransformCrop(cropDetails, gridPropertyDetails);
+        }
+
+
         Destroy(gameObject);
     }
 
@@ -173,5 +180,20 @@ public class Crop : MonoBehaviour
             }
         }
     }
+
+    private void CreateHarvestedTransformCrop(CropDetails cropDetails, GridPropertyDetails gridPropertyDetails)
+    {
+        // Update crop in grid properties
+        gridPropertyDetails.seedItemCode = cropDetails.harvestedTransformItemCode;
+        gridPropertyDetails.growthDays = 0;
+        gridPropertyDetails.daysSinceLastHarvest = -1;
+        gridPropertyDetails.daysSinceWatered = -1;
+
+        GridPropertiesManager.Instance.SetGridPropertyDetails(gridPropertyDetails.gridX, gridPropertyDetails.gridY, gridPropertyDetails);
+
+        // Display planted crop
+        GridPropertiesManager.Instance.DisplayPlantedCrop(gridPropertyDetails);
+    }
+
 
 }

c1547d73a5bfbb0c97df4d63f8cd97d2c42d7c4e
LordDarkenon <r.ager@hotmail.co.uk>
2020-06-20 15:23:31 +0100

    17.4 Add Game Clock To Save Game

--- Assets/Scripts/SaveSystem/SceneSave.cs
+++ Assets/Scripts/SaveSystem/SceneSave.cs
@@ -3,6 +3,7 @@
 [System.Serializable]
 public class SceneSave
 {
+    public Dictionary<string, int> intDictionary;
     public Dictionary<string, bool> boolDictionary;    // string key is an identifier name we choose for this list
     public Dictionary<string, string> stringDictionary;
     public Dictionary<string, Vector3Serializable> vector3Dictionary;
